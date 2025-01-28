3da8cb31658ca56e53580792a8d07ed431a42e68
yan-vinicius-pimenta-silva <yan.fedora@gmail.com>
2025-01-28 13:51:35 -0300

    fix

---
+++ project001/message.php
@@ -0,0 +1,16 @@
+<?php 
+
+  //let's get all form values
+
+  $name = $_POST['name'];
+  $email = $_POST["email"];
+  $phone = $_POST["phone"];
+  $website = $_POST["website"];
+  $message  = $_POST["message"];
+
+  echo $name;
+
+  
+
+
+?>
